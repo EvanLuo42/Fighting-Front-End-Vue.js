540f30792a521a26d1349a1f812cba1b8bcfc396
Luo_shijian <ziyun.luo@outlook.com>
2021-02-16 22:15:09 +0800

    Add:Use vant now Update:Home.vue layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-nav-bar title="首页"></van-nav-bar>
+
+    <van-cell>
+      <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
+        <van-swipe-item>1</van-swipe-item>
+        <van-swipe-item>2</van-swipe-item>
+        <van-swipe-item>3</van-swipe-item>
+        <van-swipe-item>4</van-swipe-item>
+      </van-swipe>
+    </van-cell>
+
+    <van-cell>
+      <van-grid>
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+        <van-grid-item icon="photo-o" text="文字" />
+      </van-grid>
+    </van-cell>
+
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o"><router-link to="/">Fighting</router-link></van-tabbar-item>
+      <van-tabbar-item name="count" icon="underway-o"><router-link to="/">时间</router-link></van-tabbar-item>
+      <van-tabbar-item name="study-circle" icon="comment-o"><router-link to="/">学习圈</router-link></van-tabbar-item>
+      <van-tabbar-item name="my" icon="user-o"><router-link to="/">我的</router-link></van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      active: 'home',
+    }
+  },
 }
 </script>
+
+<style>
+  .my-swipe .van-swipe-item {
+    color: #fff;
+    font-size: 20px;
+    line-height: 200px;
+    text-align: center;
+    background-color: #39a9ed;
+  }
+</style>
