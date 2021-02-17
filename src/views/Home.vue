0d9dea774e1a17bf5cfda51f104bfe2a53f04bf3
Luo_shijian <ziyun.luo@outlook.com>
2021-02-17 13:19:18 +0800

    Add:Notice function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,12 +20,20 @@
       <van-grid-item icon="photo-o" text="文字" to="/"/>
     </van-grid>
 
+    <h3>公告</h3>
+
+    <div class="notice" v-for="item in status" :key="item">
+      <van-cell :title="item.title" :value="item.date" :label="item.content" size="large" />
+    </div>
+
     <Tabbar/>
   </div>
 </template>
 
 <script>
 import Tabbar from "@/components/Tabbar";
+import axios from 'axios'
+import { Notify } from 'vant';
 
 export default {
   name: 'Home',
@@ -34,8 +42,24 @@ export default {
   },
   data() {
     return {
+      status: [
+        { title: 'v0.0.1 版本', content: '测试版本发布', date: '2020.2.17'},
+        { title: 'v0.0.2 版本', content: '测试版本发布', date: '2020.2.17'},
+      ]
     }
   },
+  mounted() {
+    axios.get('/api')
+        .then(response => {
+          response = JSON.parse(response.data);
+          for (var a in response.data.status) {
+            this.status.push(a)
+          }
+        })
+        .catch(error => {
+          Notify({ type: 'warning', message: error.toString() });
+        });
+  }
 }
 </script>
 
@@ -46,5 +70,8 @@ export default {
     line-height: 200px;
     text-align: center;
     background-color: #39a9ed;
+  }
+  h3 {
+    margin-left: 20px;
   }
 </style>
