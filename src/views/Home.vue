b93f7bca84e6bfee8299aaccf1c174bba5174cbb
EvanLuo42 <ziyun.luo@outlook.com>
2021-06-30 18:07:37 +0800

    Add Banners getter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,20 +4,19 @@
 
     <van-cell style="background-color: #F9F9F9">
       <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-        <van-swipe-item>1</van-swipe-item>
-        <van-swipe-item>2</van-swipe-item>
-        <van-swipe-item>3</van-swipe-item>
-        <van-swipe-item>4</van-swipe-item>
+        <van-swipe-item v-for="banner in banners" :key="banner">
+          <van-image :show-error="showError" :src="banner.url"/>
+        </van-swipe-item>
       </van-swipe>
     </van-cell>
 
     <div style="height: 20px;background-color: #F9F9F9"></div>
 
     <van-grid>
-      <van-grid-item icon="photo-o" text="文字" to="/"/>
-      <van-grid-item icon="photo-o" text="文字" to="/"/>
-      <van-grid-item icon="photo-o" text="文字" to="/"/>
-      <van-grid-item icon="photo-o" text="文字" to="/"/>
+      <van-grid-item icon="photo-o" text="计时器" to="/time"/>
+      <van-grid-item icon="photo-o" text="学习圈" to="/studycircle"/>
+      <van-grid-item icon="photo-o" text="装扮" to="/"/>
+      <van-grid-item icon="photo-o" text="实验室" to="/"/>
     </van-grid>
 
     <div style="height: 20px;background-color: #F9F9F9"></div>
@@ -54,45 +53,54 @@ export default {
     return {
       status: [
         { title: 'v0.0.1 版本', content: '测试版本发布', date: '2020.2.17'},
-        { title: 'v0.0.2 版本', content: '测试版本发布', date: '2020.2.17'},
-        { title: 'v0.0.3 版本', content: '测试版本发布', date: '2020.2.17'},
-        { title: 'v0.0.4 版本', content: '测试版本发布', date: '2020.2.17'},
-        { title: 'v0.0.5 版本', content: '测试版本发布', date: '2020.2.17'},
-      ]
+      ],
+      banners: [
+        { url: 'https://img01.yzcdn.cn/vant/cat.jpeg' }
+      ],
+      showError: false
     }
   },
   mounted() {
     if(!this.$cookies.isKey('user_session') || !this.$cookies.isKey('user_access')) {
       if(!this.$cookies.isKey('user_name') || !this.$cookies.isKey('user_pass')) {
-        this.$router.push({ path: '/login' })
+        //this.$router.push({ path: '/login' })
       } else {
         axios
-            .post('/api/v1/login')
+            .post('/api/login')
             .then(response => {
               if(response.data.status === 'ok') {
                 this.$cookies.set('user_session', response.data.user_session, '30min')
                 this.$cookies.set('user_access', response.data.user_access, '30min')
               } else if(response.data.status === 'error') {
-                this.$router.push({ path: '/login' })
+                //this.$router.push({ path: '/login' })
               }
             })
             .catch(function (error) {
               Notify({ type: 'warning', message: error.toString() })
+              //this.$router.push({ path: '/login' })
             })
       }
     }
+    axios
+        .get('/api/banner')
+        .then(response => {
+          if(response.data.status === 'ok') {
+            for (const banner in response.data.banners) {
+              this.banners.push(banner)
+            }
+          } else {
+            this.showError = true
+          }
+        })
+        .catch(function (error) {
+          Notify({ type: 'warning', message: error.toString() })
+          this.showError = true
+        })
   }
 }
 </script>
 
 <style>
-  .my-swipe .van-swipe-item {
-    color: #fff;
-    font-size: 20px;
-    line-height: 200px;
-    text-align: center;
-    background-color: #39a9ed;
-  }
   h3 {
     margin-left: 20px;
   }
